4bf3506e4f0022fc153ce5eacc069fa5e339a65b
Mojolagbe Jamiu <mojolagbe@gmail.com>
2016-08-31 14:17:03 -0500

    Old admin deleted

--- admin/includes/secure.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "functions.php";
-session_start();
-if($_SESSION['user']=='')
-{
-	Redirect("login.php?msg=1");
-}
-?>
--- admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "includes/functions.php";
-session_start();
-session_destroy();
-unset($_SESSION['user']);
-Redirect("login.php?msg=4");
-?>
